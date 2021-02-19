54f3637ef5c08c47b5c9bb6fde62ae9af6a9b284
Marcos S. Camargo <mscamargo.dev@gmail.com>
2021-02-18 21:51:49 -0300

    feat(shortener/entities): add url entity

---
+++ src/modules/shortener/entities/url.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Url {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  shortId: string;
+
+  @Column()
+  url: string;
+
+  @Column({ default: 0 })
+  visitCount?: number;
+}
