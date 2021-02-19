b0d62ef2bc10da788ec30411bc0659b5579a6044
Marcos S. Camargo <mscamargo.dev@gmail.com>
2021-02-19 18:04:46 -0300

    feat: change swagger path

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('docs', app, document);
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
